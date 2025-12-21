c94739122e310f2917a01e8f3972706d3becfac4
CoralJG <jacomeecoral@gmail.com>
2025-12-21 13:22:33 +0100

    Images update

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,8 +1,8 @@
-import { createClient } from 'contentful';
+/*import { createClient } from 'contentful';
 import type { Entry } from 'contentful';
 
 export const contentfulClient = createClient({
   space: import.meta.env.CONTENTFUL_SPACE_ID as string,
   accessToken: import.meta.env.CONTENTFUL_ACCESS_TOKEN as string,
   environment: 'master',
-});
+}); */
